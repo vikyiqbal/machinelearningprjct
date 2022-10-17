bb3eaac164d137b03271a6317f9b458bd681571d
vikyiqbal <85992710+vikyiqbal@users.noreply.github.com>
2022-10-17 12:09:26 +0700

    Membuat kolom pada tabel

--- BDC.py
+++ BDC.py
@@ -5,3 +5,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import statistics as stat
 sns.set()
+#membuat nama kolom
+kolom=['Nomor peserta',
+'Nomor keluarga',
+'Bobot',
+'ID Kunjungan FKTP',
+'Tanggal datang kunjungan FKTP',
+'Tanggal pulang kunjungan FKTP',
+'Provinsi FKTP',
+'Kabupaten/Kota FKTP',
+'Kepemilikan FKTP',
+'Jenis FKTP',
+'Tipe FKTP',
+'Tingkat Pelayanan FKTP',
+'Jenis Poli FKTP',
+'Segmen saat akses FKTP',
+'Kode dan nama diagnosis ICD 10',
+'Kode diagnosis ICD 10',
+'Kode diagnosis ',
+'Nama diagnosis dari FKP15',
+'Provinsi faskes tujuan rujukan',
+'Kota faskes rujukan',
+'Kepemilikan faskes rujukan',
+'Jenis faskes rujukan',
+'Tipe faskes tujuan rujukan',
+'Poli faskes tujuan rujukan',
+'Jenis Kunjungan FKTP',
+'Status Pulang Peserta']
